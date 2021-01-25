8172c3a899f814fe44b1381b1cf3d44c3c455213
loup-9 <70693150+loup-9@users.noreply.github.com>
2021-01-25 22:25:54 +0100

    add main de main
    fermeture des instances a 22h22 , yes

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region     = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  count         = var.create_instance ? var.instance_number : 0
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name = "tp_dev_ynov"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
