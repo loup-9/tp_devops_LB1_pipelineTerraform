c0422929fa482cc4260a7b667bed80af9b197a24
loup-9 <70693150+loup-9@users.noreply.github.com>
2021-01-25 22:41:43 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -4,6 +4,12 @@ variable "bucket" {
   default = "ec2_terraform"
 }
 
+
+variable "region" {
+  type = string
+  description = "AWS region"
+  default = "eu-west-3"
+}
 variable "path" {
   type = string
   description = "Terraform state path"
@@ -15,9 +21,20 @@ variable "instance_name" {
   description = "Instance name"
 }
 
-variable "region" {
+
+variable "instance_t" {
   type = string
-  description = "AWS region"
-  default = "eu-west-3"
+  description = "Instance t"
+  default = "t2.micro"
+}
+variable "instance_nbr" {
+  type = number
+  description = "Instance nbr"
+  default = 1
 }
 
+variable "create_instance" {
+  type = bool
+  description = "Create instance"
+  default = true
+}
